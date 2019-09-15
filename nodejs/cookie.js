7d127a5fad3e29dbd36ca5437dc326f93525f0d5
kkw_10 <kkw_10@naver.com>
2019-09-16 03:08:20 +0900

    기능: permanent 쿠키 만들어보기

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -11,7 +11,11 @@ http.createServer((req, res) => {
     console.log(cookies);
 
     res.writeHead(200, {
-        'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
+        'Set-Cookie': [
+            'yummy_cookie=choco', 
+            'tasty_cookie=strawberry',
+            `Permanent=cookies; Max-Age=${60*60*24*30}`
+        ]
     })
 
     res.end('cookie!!');
