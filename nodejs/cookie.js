18e914c720173d07d72dac401f9168f54611b123
kkw_10 <kkw_10@naver.com>
2019-09-16 02:43:44 +0900

    기능: cookie 모듈 설치 및 활용

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -1,8 +1,19 @@
 const http = require('http');
+const cookie = require('cookie'); // request 헤더에 있는 쿠키정보를 사용하기 쉽게 객체로 만들어줌
 
 http.createServer((req, res) => {
+    let cookies = {};
+
+    if(req.headers.cookie !== undefined) {
+        cookies = cookie.parse(req.headers.cookie);
+    }
+    
+    console.log(cookies);
+
     res.writeHead(200, {
         'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
     })
+
     res.end('cookie!!');
+
 }).listen(3000);
